335d49302a54344e6b34ff25de56bcbf002ba549
David Byrd <dbyrd@depparts.com>
2019-04-13 12:51:10 -0400

    logo

--- bracketbuilder/src/components/NavTabs.js
+++ bracketbuilder/src/components/NavTabs.js
@@ -3,6 +3,10 @@ import { Link } from "react-router-dom";
 
 function NavTabs() {
   return (
+    <nav className="navbar navbar-light bg-light">
+    <a className="navbar-brand" href="/">
+        <img src={process.env.PUBLIC_URL + '/favicon.ico'} width="40" height="30" vspace="5" hspace="5" alt=""></img>
+      </a>
     <ul className="nav nav-tabs">
       <li className="nav-item">
         <Link to="/" className={window.location.pathname === "/" ? "nav-link active" : "nav-link"}>
@@ -26,6 +30,7 @@ function NavTabs() {
         </Link>
       </li>
     </ul>
+    </nav>
   );
 }
 
